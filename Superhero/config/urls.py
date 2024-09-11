bf93eae203d1648d1dd384121bd857674748b49f
Sam Polachek <pola3704@bears.unco.edu>
2024-09-11 15:55:57 -0600

    SuperheroProject

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from hero.views import HulkView, IronManView
+from hero.views import BlackWidowView, HulkView, IndexView, IronManView
 
 urlpatterns = [
     path('hulk',        HulkView.as_view()),
     path('ironman',     IronManView.as_view()),
+    path('blackwidow',  BlackWidowView.as_view()),
+    path('',            IndexView.as_view()),
 ]
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -19,3 +19,16 @@ class IronManView(TemplateView):
             'id': 'Tony Stark',
             'image': '/static/images/iron_man.jpg'
         }
+    
+class BlackWidowView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Black Widow',
+            'id': 'Natasha Romanov',
+            'image': '/static/images/black_widow.jpg'
+        }
+    
+class IndexView(TemplateView):
+    template_name = 'heroes.html'
