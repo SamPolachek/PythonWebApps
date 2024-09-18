1967495e8180861ccc5b9dabf82b690a05b349a1
Sam Polachek <pola3704@bears.unco.edu>
2024-09-18 17:54:41 -0600

    Project 3 Update

--- Gallery/photos/views.py
+++ Gallery/photos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class PhotoView(TemplateView):
+    template_name = 'photo.html'
+    def get_context_data(self, **kwargs):
+        name = kwargs['name']
+        image = f'static/images/chapter-1.jpg'
+        return {'photo': "static/images/chapter-1.jpg"}
