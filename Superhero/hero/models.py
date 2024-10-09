64aa8b2bc947c09157a5837458589447fb5b4f1a
Sam Polachek <pola3704@bears.unco.edu>
2024-10-09 15:03:24 -0600

    Begin Project 6

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -14,3 +14,10 @@ class Superhero(models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('hero_list')
+    
+class Article (models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    hero = models.ForeignKey(Superhero, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
